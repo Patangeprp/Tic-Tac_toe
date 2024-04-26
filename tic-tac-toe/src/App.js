bfd1aa87495d179b7b5a58af63330a73106cdb50
prathmesh_patange <prathmeshpatange123@gmail.com>
2024-04-26 13:56:52 +0530

    game implemented successfully

---
+++ tic-tac-toe/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Tictactoe from './components/tictactoe/Tictactoe';
+
+function App() {
+  return (
+    <div className="App">
+      <Tictactoe/>
+    </div>
+  );
+}
+
+export default App;
